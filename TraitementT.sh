8d42e4b3158efaf9d046028ac9d4a26d1260a3bd
Myri23 <153503012+Myri23@users.noreply.github.com>
2024-01-19 09:12:42 +0100

    Add files via upload

--- TraitementT.sh
+++ TraitementT.sh
@@ -6,13 +6,12 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
  
-cut -d";" -f1,3 data.csv > tempT1.txt
-cut -d";" -f1,4 data.csv > tempT2.txt
-cut -d";" -f1,3,4 data.csv > tempTbis.csv
-paste -d '\n' tempT1.txt tempT2.txt > tempT3.txt
-uniq -c tempT3.txt > tempT4.txt
+cut -d";" -f1,3 data.csv | paste -d '\n'-<(cut -d";" -f1,4 data.csv) | sort | uniq -c > tempT3.txt 
+
 
 head -10 resultatsTc.txt > resultatsT1.csv
+
+cut -d";" -f1,3,4 data.csv > tempTbis.csv
 
 # Assignation des noms de fichiers aux variables
 data_csv="tempTbis.csv"
