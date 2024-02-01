5e6fd22ef85a148c22ccdc235cedea3191e23b8c
naelaben <106915601+naelaben@users.noreply.github.com>
2024-02-01 22:24:19 +0100

    Update TraitementD1.sh

--- TraitementD1.sh
+++ TraitementD1.sh
@@ -15,3 +15,34 @@ duration=$((end - start))
 
 # Affichage de la durée
 echo "Durée d'exécution : $duration secondes"
+
+# Appeler le script Gnuplot pour générer le graphique
+gnuplot -persist << GNU_CMD
+# Spécifier le terminal de sortie
+set terminal png
+
+# Spécifier le nom du fichier de sortie
+set output "histogramme_horizontal_D1.png"
+
+# Style de remplissage des barres
+set style fill solid
+
+# Largeur des barres
+set boxwidth 2
+
+# Titre du graphique
+set ylabel "Conducteurs avec le plus de trajets (option -d1)"
+
+# Étiquettes des axes
+set y2tics
+set xlabel "Noms des Conducteurs"
+set y2label "Nombre de Trajets"
+set xtics rotate by 90 right
+set ytics rotate by 90 right
+
+# Définir une plage pour l'axe y2
+set y2range [0:0]
+
+# Tracer l'histogramme horizontal à partir d'un fichier de données
+plot "temp/result_D1.txt" using 1:xtic(3) with histograms title "Nombre de Trajets"
+GNU_CMD
