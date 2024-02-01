986bf85b33ba3c5f0c78de9b74e9512ae6706a82
MehdiSek <134293726+MehdiSek@users.noreply.github.com>
2024-02-01 16:43:24 +0100

    Update avlS.h

--- avlS.h
+++ avlS.h
@@ -25,5 +25,22 @@ typedef struct AVL {
 } AVL;
 
 AVL* creerAVL(TRAJET t);
+int estVide(AVL* p);
+int estFeuille(AVL* p);
+TRAJET avoirTrajet(AVL* p);
+int aFilsGauche(AVL* p);
+int aFilsDroit(AVL* p);
+int recherche(AVL* p, TRAJET t);
+AVL* insertionAVL(AVL* pAVL, TRAJET t, int* h);
+void ajoutFilsDroit(AVL* p, TRAJET t);
+void ajoutFilsGauche(AVL* p, TRAJET t);
+AVL* RotationGauche(AVL* pAVL);
+AVL* RotationDroite(AVL* pAVL);
+AVL* DoubleRotationGauche(AVL* pAVL);
+AVL* DoubleRotationDroite(AVL* pAVL);
+AVL* equilibrerAVL(AVL* pAVL);
+AVL* suppMinAVL(AVL* pAVL, int* h, TRAJET* pt);
+AVL* suppressionAVL(AVL* pAVL, TRAJET t, int* h);
+void infixeFichier(AVL *p, FILE *f);
 
 #endif /* AVLT_H */
