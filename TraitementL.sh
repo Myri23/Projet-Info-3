ee87b33abd994f797631c4c385bc1d9044dd257b
naelaben <106915601+naelaben@users.noreply.github.com>
2024-02-01 22:33:14 +0100

    Update TraitementL.sh

--- TraitementL.sh
+++ TraitementL.sh
@@ -15,3 +15,29 @@ duration=$((end - start))
 
 # Affichage de la durée
 echo "Durée d'exécution : $duration secondes"
+
+# Appeler le script Gnuplot pour générer le graphique
+gnuplot -persist << GNU_CMD
+# Spécifier le terminal de sortie
+set terminal png
+
+# Spécifier le nom du fichier de sortie
+set output "histogramme_L.png"
+
+# Style de remplissage des barres
+set style fill solid
+
+# Largeur des barres
+set boxwidth 1
+
+# Titre du graphique
+set title "Les 10 trajets les plus longs"
+
+# Étiquettes des axes
+set xlabel "Route ID"
+set ylabel "Distances (km)"
+set xtics rotate by 90 right
+
+# Tracer l'histogramme horizontal à partir d'un fichier de données
+plot "./temp/result_L2.txt" using 2:xtic(1) with histograms notitle
+GNU_CMD
