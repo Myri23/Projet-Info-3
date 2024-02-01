70dd700b0b8d875f2ad9625a9608f64a9b5d3aca
naelaben <106915601+naelaben@users.noreply.github.com>
2024-02-01 22:26:33 +0100

    Update TraitementD2.sh

--- TraitementD2.sh
+++ TraitementD2.sh
@@ -22,3 +22,29 @@ duration=$((end - start))
 
 # Affichage de la durée
 echo "Durée d'exécution : $duration secondes"
+
+# Appeler le script Gnuplot pour générer le graphique
+gnuplot -persist << GNU_CMD
+# Spécifier le terminal de sortie
+set terminal png
+
+# Spécifier le nom du fichier de sortie
+set output "histogramme_horizontal_D2.png"
+
+# Style de remplissage des barres
+set style fill solid
+
+# Largeur des barres
+set boxwidth 2
+
+# Titre du graphique
+set ylabel "Conducteurs avec plus longue distance (option -d2)"
+
+# Étiquettes des axes
+set xlabel "Noms des Conducteurs"
+set y2label "Nombre de Trajets"
+set xtics rotate by 90 right
+
+# Tracer l'histogramme horizontal à partir d'un fichier de données
+plot "temp/result_D2.txt" using 3:xtic(2) with histograms title "Nombre de Trajets"
+GNU_CMD
