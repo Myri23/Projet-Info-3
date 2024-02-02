0e700bed4a90c6b2f7ea3df1d3c7f48c6f0217a8
MehdiSek <134293726+MehdiSek@users.noreply.github.com>
2024-02-02 18:40:48 +0100

    Update TraitementD1.sh

--- TraitementD1.sh
+++ TraitementD1.sh
@@ -1,55 +1,52 @@
 #!/bin/bash
 
-# Début du chronomètre
-start=$(date +%s)
+
+start=$(date +%s)  #Début du chronomètre
 
 cut -d";" -f1,6 data.csv | sort | uniq | cut -d";" -f2 | sort | uniq -c | sort -r -k1 | head -10 > demo/result_D1.txt
 
-# Appeler le script Gnuplot pour générer le graphique
-gnuplot -persist << GNU_CMD
-# Spécifier le terminal de sortie
-set terminal png
 
-# Spécifier le nom du fichier de sortie
-set output "images/histogramme_horizontal_D1.png"
+gnuplot -persist << GNU_CMD  #Appeler le script Gnuplot pour générer le graphique
 
-# Style de remplissage des barres
-set style fill solid
+set terminal png  #Spécifier le terminal de sortie
 
-# Largeur des barres
-set boxwidth 2
+set output "images/histogramme_horizontal_D1.png"  #Spécifier le nom du fichier de sortie
 
-# Titre du graphique
-set ylabel "Conducteurs avec le plus de trajets (option -d1)"
 
-# Étiquettes des axes
-set y2tics
+set style fill solid  #Style de remplissage des barres
+
+
+set boxwidth 2  #Largeur des barres
+
+
+set ylabel "Conducteurs avec le plus de trajets (option -d1)"  #Titre du graphique
+
+
+set y2tics # Étiquettes des axes
 set xlabel "Nombre de Trajets"
 set y2label "Noms des Conducteurs"
 set xtics rotate by 90 right
 set y2tics rotate by 90 right
 unset ytics
 
-# Obtenir la valeur maximale de la colonne 1
-stats "demo/result_D1.txt" using 1 nooutput
+stats "demo/result_D1.txt" using 1 nooutput # Obtenir la valeur maximale de la colonne 1
 max_value = STATS_max + 50
 
-# Définir une plage pour l'axe y
-set y2range [0:max_value]
 
-# Tracer l'histogramme horizontal à partir d'un fichier de données
-plot "demo/result_D1.txt" using 1:xticlabels(strcol(2) . " " . strcol(3)) with histograms title "Nombre de Trajets"
+set y2range [0:max_value] # Définir une plage pour l'axe y
+
+plot "demo/result_D1.txt" using 1:xticlabels(strcol(2) . " " . strcol(3)) with histograms title "Nombre de Trajets" # Tracer l'histogramme horizontal à partir d'un fichier de données
 
 
 
 GNU_CMD
 
 
-# Fin du chronomètre
-end=$(date +%s)
 
-# Calcul de la durée d'exécution en secondes
-duration=$((end - start))
+end=$(date +%s) # Fin du chronomètre
 
-# Affichage de la durée
-echo "Durée d'exécution : $duration secondes"
+
+duration=$((end - start)) # Calcul de la durée d'exécution en secondes
+
+
+echo "Durée d'exécution : $duration secondes" # Affichage de la durée
