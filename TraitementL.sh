3ceb718987297eb7f66ee8a3386af012459f1b9f
naelaben <106915601+naelaben@users.noreply.github.com>
2024-02-02 18:42:42 +0100

    Update TraitementL.sh

--- TraitementL.sh
+++ TraitementL.sh
@@ -1,51 +1,37 @@
 #!/bin/bash 
 
-# Début du chronomètre
-start=$(date +%s)
+start=$(date +%s) # Début du chronomètre
 
 cut -d";" -f1,5 data.csv | awk -F';' '{ distances[$1]+=$2 } END { for (trajet in distances) print trajet " " distances[trajet] }' > temp/result_L2.txt
 
 sort -nr -t' ' -k2 temp/result_L2.txt | head -10 | sort -rn -t' ' -k1 > demo/result_L.txt
 
 
+gnuplot -persist << GNU_CMD # Appeler le script Gnuplot pour générer le graphique
+
+set terminal png # terminal de sortie
+
+set output "images/histogramme_L.png" # le nom du fichier de sortie
+
+set style fill solid # Style de remplissage des barres
+
+set boxwidth 1 # Largeur des barres
+
+set title "Les 10 trajets les plus longs (option -l)" # Titre du graphique
 
 
-# Appeler le script Gnuplot pour générer le graphique
-gnuplot -persist << GNU_CMD
-# Spécifier le terminal de sortie
-set terminal png
-
-# Spécifier le nom du fichier de sortie
-set output "images/histogramme_L.png"
-
-# Style de remplissage des barres
-set style fill solid
-
-# Largeur des barres
-set boxwidth 1
-
-# Titre du graphique
-set title "Les 10 trajets les plus longs (option -l)"
-
-# Étiquettes des axes
-set xlabel "Route ID"
+set xlabel "Route ID" # Étiquettes des axes
 set ylabel "Distances (km)"
 set xtics rotate by 90 right
 
-
-# Tracer l'histogramme horizontal à partir d'un fichier de données
-plot "./demo/result_L.txt" using 2:xticlabels(1) with histograms notitle
+plot "./demo/result_L.txt" using 2:xticlabels(1) with histograms notitle # Tracer l'histogramme horizontal à partir de result_L.txt
 GNU_CMD
 
 
+end=$(date +%s) #Fin du chronomètre
 
-#Fin du chronomètre
-end=$(date +%s)
+duration=$((end - start)) # Calcul de la durée d'exécution en secondes
 
-# Calcul de la durée d'exécution en secondes
-duration=$((end - start))
-
-# Affichage de la durée
-echo "Durée d'exécution : $duration secondes"
+echo "Durée d'exécution : $duration secondes" # Affichage de la durée
 
 
